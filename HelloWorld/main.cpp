49901858f588980a154606d7cbd1a8f049242904
BAKHTAWAR <BNOOR23@GMAIL.COM>
2017-11-26 12:43:30 +0500

    change added in main cpp file

--- HelloWorld/main.cpp
+++ HelloWorld/main.cpp
@@ -8,6 +8,8 @@ int main()
 	cout << "Enter the radius of circle " << endl;
 	cin >> radius;
 	cout << " The area of circle is " << CircleArea(radius);
+	cout << endl;
+	cout<<"It is now published on github ";
 	system("pause");
 
 	return 0;
